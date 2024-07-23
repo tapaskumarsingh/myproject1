9b8ef987fce0c5cdf62445bb158cc3b81e66acaa
tapaskumarsingh <tapassinghrajput19@gmail.com>
2024-07-23 15:16:05 +0530

    first commit

---
+++ JS/tapas/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Home'
+
+export const App = () => { /* Export mean to send  files */
+  return (
+
+    <>
+    <div><h1>Tapas</h1></div>
+    <p>This is Tapas</p>
+    </>
+  )
+}
+
+export default App;
